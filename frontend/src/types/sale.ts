cef8e4d1972168d349cfbcca698ac7c32d601d20
Fabiano Gois <fabiiano.gois@gmail.com>
2021-11-07 22:51:46 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,9 @@
 export type SaleSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
+}
+export type SaleSuccess = {
+    sellerName: String;
+    visited: number;
+    deals: number;
 }
